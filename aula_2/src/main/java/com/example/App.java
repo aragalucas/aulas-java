35db3efd77924c1bbeeda0b922804a2a770ff6f0
aragalucas <126295197+aragalucas@users.noreply.github.com>
2024-08-05 14:08:24 +0000

    exercicio da segunda aula

---
+++ aula_2/src/main/java/com/example/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        boolean temSol = true;
+        boolean folga = true;
+
+        if (temSol && folga) {
+            System.out.println("Irei passear hoje :D");
+         } else {
+            System.out.println("não irei passear =(" );
+         }
+    }
+}
